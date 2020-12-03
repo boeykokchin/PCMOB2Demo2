76d32d6d57695c36e391a10da2a8a690cd13103c
Boey Kok Chin <boeykokchin@live.com.sg>
2020-12-04 01:16:12 +0800

    based - part 1 OK

--- App.js
+++ App.js
@@ -5,16 +5,63 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import EventsScreen from './screens/EventsScreen';
 import ContactScreen from './screens/ContactScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+// import { Ionicons } from '@expo/vector-icons';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Events' component={EventsScreen} />
-        <Tab.Screen name='Contact' component={ContactScreen} />
+      <Tab.Navigator
+        initialRouteName='Home'
+        tabBarOptions={{
+          activeTintColor: 'orange',
+        }}
+      >
+        <Tab.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='home-outline'
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Events'
+          component={EventsScreen}
+          options={{
+            tabBarLabel: 'Events',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='calendar-multiple-check'
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Contact'
+          component={ContactScreen}
+          options={{
+            tabBarLabel: 'Contact',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='contacts'
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
